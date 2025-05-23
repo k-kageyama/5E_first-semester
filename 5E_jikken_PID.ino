2775c8552cbdec9a57389ba43a3e94d9afb693d7
k-kageyama <82993468+k-kageyama@users.noreply.github.com>
2025-05-23 11:31:20 +0900

    Serial Printのエラー修正　一部コメント追加

--- 5E_jikken_PID.ino
+++ 5E_jikken_PID.ino
@@ -1,20 +1,28 @@
+/*
+PIDシステムプログラム
+
+参考文献
+温度センサMAX6675の温度データ読み取り
+https://omoroya.com/arduino-lesson70/
+*/
+
 #include <SPI.h>
 int value; // MAX6675からのデータ格納変数
 int outputPin = 4;
-//on_time off_time ヒータ稼働時間設定変数
-//on_time 0~1000 
+//on_time ヒータONの時間　off_time ヒータOFFの時間
 double on_time;
 double off_time;
 
 //PID制御　パラメータ
 //Kp,Ki,Kd ゲイン
+//必要に応じて　int , double型を切り替える
 int Kp = 1;
 int Ki = 0;
 int Kd = 0;
 double P;
 double I;
 double D;
-double target = 60.0; //目標温度
+double target = 50.0; //目標温度
 double temp; //センサーが読み取る温度
 double mv; //操作量
 void setup() {
@@ -49,15 +57,12 @@ void loop() {
   //読み出した温度の表示 bit<14:3>が温度数値格納bit bit<15>=0(dummy bit)
   } else {
     //Serial.print((value >> 3), BIN); //bit<15:3>のみ2進表示
-    //Serial.print(", ");
-    //温度データを出力
-    Serial.print((value >> 3) * 0.25); //bit<15:3>のみ表示、MAX6675仕様(分解能 0.25℃)
     //温度を取得
     temp = ((value >> 3) * 0.25);
-    //Serial.println(" degC");
+    Serial.print(", ");
   } 
 
-
+  //**********の部分を各自考えて、書き換える
   //制御量の計算
   P = *************
   //I = *************
@@ -74,13 +79,13 @@ void loop() {
     on_time = *************
     off_time = 1000.0 - on_time;
   }
-
-
+//delay()の中にマイナスの値が入るとプログラムが停止するので注意！
   digitalWrite(outputPin, HIGH);
   delay((int)on_time);
   digitalWrite(outputPin, LOW);
   delay((int)off_time);
 
+//シリアルモニタに表示させたいデータをここに追加
   Serial.print(Kp);
   Serial.print(", ");
   Serial.print(mv);
